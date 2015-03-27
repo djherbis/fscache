855ff85432f26ce662ff5a6a2fd38f076e25d654
djherbis <djherbis@gmail.com>
2015-03-26 22:07:17 -0400

    added a test for concurrent writer/reader, also exists

--- fscache_test.go
+++ fscache_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"io"
 	"testing"
+	"time"
 )
 
 func TestSanity(t *testing.T) {
@@ -32,5 +33,41 @@ func TestSanity(t *testing.T) {
 	}
 	if !bytes.Equal(buf.Bytes(), []byte("hello world\n")) {
 		t.Errorf("unexpected output %s", buf.Bytes())
+	}
+}
+
+func TestConcurrent(t *testing.T) {
+	c, err := New("./cache", 0700, 0)
+	if err != nil {
+		t.Error(err.Error())
+		return
+	}
+	defer c.Clean()
+
+	r, w, err := c.Get("stream")
+	r.Close()
+	if err != nil {
+		t.Error(err.Error())
+		return
+	}
+	go func() {
+		w.Write([]byte("hello"))
+		<-time.After(100 * time.Millisecond)
+		w.Write([]byte("world"))
+		w.Close()
+	}()
+
+	if c.Exists("stream") {
+		r, _, err := c.Get("stream")
+		defer r.Close()
+		if err != nil {
+			t.Error(err.Error())
+			return
+		}
+		buf := bytes.NewBuffer(nil)
+		io.Copy(buf, r)
+		if !bytes.Equal(buf.Bytes(), []byte("helloworld")) {
+			t.Errorf("unexpected output %s", buf.Bytes())
+		}
 	}
 }
