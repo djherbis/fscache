e9f453d1a254439560463da26684869588208434
djherbis <djherbis@gmail.com>
2015-03-26 20:46:49 -0400

    added a simple test case

---
+++ fscache_test.go
@@ -0,0 +1,30 @@
+package fscache
+
+import (
+	"bytes"
+	"io"
+	"testing"
+)
+
+func TestSanity(t *testing.T) {
+	c, err := New("./cache", 0666, 0)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	defer c.Clean()
+
+	r, w, err := c.Get("stream")
+	defer r.Close()
+
+	w.Write([]byte("hello world\n"))
+	w.Close()
+
+	buf := bytes.NewBuffer(nil)
+	_, err = io.Copy(buf, r)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	if !bytes.Equal(buf.Bytes(), []byte("hello world\n")) {
+		t.Errorf("unexpected output %s", buf.Bytes())
+	}
+}
