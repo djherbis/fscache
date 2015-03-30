263a99caa633375ac485e815921a362415abad9d
djherbis <djherbis@gmail.com>
2015-03-30 00:35:17 -0400

    handler now caches response header

--- handler.go
+++ handler.go
@@ -1,8 +1,12 @@
 package fscache
 
 import (
+	"bytes"
+	"encoding/gob"
+	"fmt"
 	"io"
 	"net/http"
+	"sync"
 )
 
 // Handler is a caching middle-ware for http Handlers.
@@ -10,7 +14,8 @@ import (
 // using the passed cache. The cache key for the request is the req.URL.String().
 func Handler(c *Cache, h http.Handler) http.Handler {
 	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		r, w, err := c.Get(req.URL.String())
+		url := req.URL.String()
+		r, w, err := c.Get(url)
 		if err != nil {
 			h.ServeHTTP(rw, req)
 			return
@@ -25,15 +30,43 @@ func Handler(c *Cache, h http.Handler) http.Handler {
 				}, req)
 			}()
 		}
+		decodeHeader(r, rw.Header())
 		io.Copy(rw, r)
 	})
+}
+
+func encodeHeader(w io.Writer, h http.Header) error {
+	buf := bytes.NewBuffer(nil)
+	enc := gob.NewEncoder(buf)
+	err := enc.Encode(h)
+	if err != nil {
+		return err
+	}
+	_, err = fmt.Fprintln(w, int64(buf.Len()))
+	if err != nil {
+		return err
+	}
+	_, err = io.Copy(w, buf)
+	return err
+}
+
+func decodeHeader(r io.Reader, h http.Header) error {
+	var size int64
+	_, err := fmt.Fscanln(r, &size)
+	if err != nil {
+		return err
+	}
+	dec := gob.NewDecoder(io.LimitReader(r, size))
+	return dec.Decode(&h)
 }
 
 type respWrapper struct {
 	http.ResponseWriter
 	io.Writer
+	sync.Once
 }
 
 func (r *respWrapper) Write(p []byte) (int, error) {
+	r.Do(func() { encodeHeader(r.Writer, r.Header()) })
 	return r.Writer.Write(p)
 }
